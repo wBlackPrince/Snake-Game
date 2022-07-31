8c58942749db4eb70200cbf4a8ea5361ac7a1f1f
wBlackPrince <eduard.nikitin.04@list.ru>
2022-07-31 14:57:42 +0300

    Добавление возможности перезапуска игры

--- game.py
+++ game.py
@@ -19,7 +19,8 @@ def update():
         s.game_over = True
     
     if s.game_over:
-        print_on_screen('GAME OVER',position = (-.1,0))
+        print_on_screen('GAME OVER',position = (-.1,.1))
+        restart_button.enabled = True
         destroy(s)
         for i in range(len(s.bodies)):
             destroy(s.bodies[i])
@@ -27,9 +28,22 @@ def update():
     if len(apples) == 0:
         apples = [Apple() for i in range(4)]
 
+def click_restart():
+    global s,apples
+    #? уничтожение объектов сцены
+    for i in range(len(apples)):
+        destroy(apples[i])
+    
+    #? переопределение игровых объектов
+    s = Snake()
+    apples = [Apple() for i in range(4)]
+
+    restart_button.enabled = False
+
 s = Snake()
 apples = [Apple() for i in range(4)]
 score = Inst_button()
 
+restart_button = Button(text = 'Restart',scale_x = .3,scale_y = .07,y = .03,x = -.03,enabled = False,color = color.gray,on_click = click_restart)
 
 app.run()
